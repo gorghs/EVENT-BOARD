2f6b4618940eae7fbe19084a1b6565f1bf2e6952
karthick <karthick.venkatachalem@gmail.com>
2025-10-10 19:18:09 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://event-board-api.onrender.com',
 });
 
 // Attach Authorization header from localStorage token
